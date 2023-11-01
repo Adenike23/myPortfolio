9bda97a6d1bac5b7d5ab23c8780b05d3178c155e
Adenike23 <adeofeadenike@gmail.com>
2023-11-02 00:14:02 +0100

    media queries updated

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ let typed = new Typed(".typing-effect", {
     typeSpeed: "9",
     loop: true,
   });
+
+  const year = new Date().getFullYear();
+  document.querySelector('.date').textContent = year
+
+const openNav = document.querySelector(".open-menu");
+const closeNav = document.querySelector(".close-menu");
+const navList = document.querySelector("nav ul");
+openNav.addEventListener("click", function() {
+  navList.style.right = "0%"
+  openNav.style.display = "none"
+  closeNav.style.display = "block"
+})
+closeNav.addEventListener("click", function() {
+  navList.style.right = "-200%"
+  openNav.style.display = "block"
+  closeNav.style.display = "none"
+})
+document.querySelectorAll("nav ul a").forEach(navLink => {
+  navLink.addEventListener("click", function() {
+      navList.style.right = "-200%"
+      openNav.style.display = "block"
+      closeNav.style.display = "none"
+  })
+})

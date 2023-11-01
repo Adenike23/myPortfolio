e1454e389d4b4cc5e25200853cab7d894c79d71d
Adenike23 <adeofeadenike@gmail.com>
2023-11-01 19:16:12 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// typed effect
+let typed = new Typed(".typing-effect", {
+    strings: ["a Front-end Developer", "an Avid Writer", " a Cue Adventurer"],
+    backSpeed: "9",
+    typeSpeed: "9",
+    loop: true,
+  });
